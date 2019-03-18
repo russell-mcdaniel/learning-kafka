f537880581b300278acf3213c203a8bbe32bb255
Russell McDaniel <rmcdaniel@siliconesis.com>
2019-03-17 19:47:57 -0700

    Implement broker list

--- src/Learning.Kafka.TestClient.ConsoleHost/Broker/ManageProcessor.cs
+++ src/Learning.Kafka.TestClient.ConsoleHost/Broker/ManageProcessor.cs
@@ -6,6 +6,8 @@ namespace Learning.Kafka.TestClient.ConsoleHost.Broker
 {
     internal static class ManageProcessor
     {
+        private static TimeSpan _requestTimeout = TimeSpan.FromSeconds(3);
+
         public static void Execute(ManageOptions options)
         {
             switch (options.Scope)
@@ -38,6 +40,30 @@ namespace Learning.Kafka.TestClient.ConsoleHost.Broker
 
         private static void ExecuteBrokerList(ManageOptions options)
         {
+            using (var client = new AdminClientBuilder(new AdminClientConfig() { BootstrapServers = options.BootstrapServer }).Build())
+            {
+                var metadata = client.GetMetadata(_requestTimeout);
+
+                Console.WriteLine("Originating Broker Information");
+                Console.WriteLine();
+                Console.WriteLine($"Broker ID:          {metadata.OriginatingBrokerId}");
+                Console.WriteLine($"Broker Name:        {metadata.OriginatingBrokerName}");
+                Console.WriteLine($"Local Topics:       {metadata.Topics.Count}");
+                Console.WriteLine($"Brokers (Cluster):  {metadata.Brokers.Count}");
+                Console.WriteLine();
+
+                Console.WriteLine("Broker List");
+                Console.WriteLine();
+                Console.WriteLine("ID                   Host                 Port");
+                Console.WriteLine("-------------------- -------------------- ----------");
+
+                foreach (var broker in metadata.Brokers)
+                {
+                    Console.WriteLine($"{broker.BrokerId,-20} {broker.Host,-20} {broker.Port}");
+                }
+
+                Console.WriteLine();
+            }
         }
 
         private static void ExecuteTopic(ManageOptions options)
